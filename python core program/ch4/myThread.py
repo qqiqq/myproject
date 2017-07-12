1412ba535c608733e4a71f2e5b0738a438014f00
Vagrant Zhou <vagrantxiang@hotmail.com>
2017-07-13 02:05:29 +0800

    添加第四章关于threading模块的测试脚本

---
+++ python core program/ch4/myThread.py
@@ -0,0 +1,20 @@
+import threading
+from time import sleep,ctime
+
+
+
+class MyThread(threading.Thread):
+    def __init__(self,func,args,name=''):
+        threading.Thread.__init__(self)
+        self.func = func
+        self.name = name
+        self.args = args
+
+    def run(self):
+        print 'starting ',self.name,' at: ',ctime()
+        self.res = self.func(*self.args)
+        print self.name,' finished at: ',ctime()
+
+    def getResult(self):
+        return self.res
+
