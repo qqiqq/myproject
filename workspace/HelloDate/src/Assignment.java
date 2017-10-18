328a608835742347be3d7d925fca4ef79b1d4271
Vagrant Zhou <vagrantxiang@hotmail.com>
2017-10-18 21:03:47 +0800

    添加java workspace，关于java编程思想，20171018版本

---
+++ workspace/HelloDate/src/Assignment.java
@@ -0,0 +1,18 @@
+class Number {
+	int i;
+}
+
+public class Assignment {
+	public static void main(String[] args){
+		Number n1 = new Number();
+		Number n2 = new Number();
+		n1.i = 9;
+		n2.i = 21;
+		System.out.println("1: n1.i: "+n1.i+", n2.i: "+n2.i);
+		n1 = n2;
+		System.out.println("2: n1.i: "+n1.i+", n2.i: "+n2.i);
+		n1.i = 37;
+		System.out.println("3: n1.i: "+n1.i+", n2.i: "+n2.i);
+	}
+
+}///:~
---
+++ workspace/HelloDate/src/sString.java
@@ -0,0 +1,9 @@
+
+public class sString {
+	public static void main(String[] args){
+		int x=0,y=12,z=7;
+		String str="x+y+z";
+		System.out.println(x+y+z+str);
+	}
+
+}
