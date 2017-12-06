daff78d47191e01ef80e830a2be04bdbfe32bf98
Vagrant Zhou <vagrantxiang@hotmail.com>
2017-12-06 11:04:56 +0800

    添加第九章爬虫脚本

--- python core program/ch9/crawl.py
+++ python core program/ch9/crawl.py
@@ -7,6 +7,17 @@ import sys
 import urllib
 import urlparse
 
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print "Encountered a start tag:", tag
+
+    def handle_endtag(self, tag):
+        print "Encountered an end tag :", tag
+
+    def handle_data(self, data):
+        print "Encountered some data  :", data
+
+
 class Retriever(object):
     __slots__ = ('url','file')
     def __init__(self,url):
